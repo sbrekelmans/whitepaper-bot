0c06db647675dbb3c6b2de3980b4985c26a27500
sbrekelmans <svenbrekelmans@gmail.com>
2017-11-06 11:23:55 +0100

    drie keer reactie van user_join bug eruit gehaald hopelijk en tekst aangepast

--- skills/sample_events.js
+++ skills/sample_events.js
@@ -2,7 +2,7 @@ module.exports = function(controller) {
 
     controller.on('user_channel_join', function(bot, message) {
 
-        bot.reply(message, 'Welkom, <@' + message.user + '> bij voorbaat excuses voor mijn gedrag, ik heb geen flauw idee wat onder of boven is.');
+        bot.reply(message, 'Welkom , <@' + message.user + '> bij voorbaat excuses voor mijn gedrag, ik heb geen flauw idee wat onder of boven is.');
 
     });
 
