3cd512cd5ecae0c283da3b0cb6c9e0244acb3404
sbrekelmans <svenbrekelmans@gmail.com>
2017-11-06 11:20:43 +0100

    drie keer reactie van user_join bug eruit gehaald hopelijk

--- skills/sample_events.js
+++ skills/sample_events.js
@@ -1,8 +1,8 @@
 module.exports = function(controller) {
 
-    controller.on('user_channel_join,user_group_join', function(bot, message) {
+    controller.on('user_channel_join', function(bot, message) {
 
-        bot.reply(message, 'Welkom, <@' + message.user + '>');
+        bot.reply(message, 'Welkom, <@' + message.user + '> bij voorbaat excuses voor mijn gedrag, ik heb geen flauw idee wat onder of boven is.');
 
     });
 
